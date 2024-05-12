49ca40b4f72fd67231bba780a37d060eae5cb0ee
wesleydavid <123563969+zWesleyDavid@users.noreply.github.com>
2024-05-11 22:39:35 -0300

    Add files via upload

---
+++ todo-list/script/script.js
@@ -0,0 +1,85 @@
+const localStorageKey = "tarefa-key"
+
+document.addEventListener("DOMContentLoaded", () => {
+    initializeLocalStorage()
+})
+
+function formatDate(date) {
+    const formattedDate = new Intl.DateTimeFormat("pt-BR", {
+        day: "numeric",
+        month: "numeric",
+        year: "numeric",
+        hour: "numeric",
+        minute: "numeric"
+    }).format(date)
+
+    return formattedDate
+}
+
+function initializeLocalStorage() {
+    const localStorageValue = localStorage.getItem(localStorageKey)
+    const currentDate = new Date()
+    if (!localStorageValue) {
+        const newValue = {
+            count: 1,
+            lastVisit: formatDate(currentDate)
+        }
+
+        const stringedValue = JSON.stringify(newValue)
+        localStorage.setItem(localStorageKey, stringedValue)
+    }
+
+    const lsValueObj = JSON.parse(localStorageValue)
+
+    lsValueObj.count++
+    lsValueObj.lastVisit = formatDate(currentDate)
+
+    localStorage.setItem(localStorageKey, JSON.stringify(lsValueObj))
+
+    const footer = document.querySelector('footer')
+    const p = document.createElement('p')
+
+    p.textContent = `Esta página foi visitada ${lsValueObj.count} vezes. A última visita foi: ${lsValueObj.lastVisit}`
+    footer.appendChild(p)
+}
+
+//document.addEventListener('DOMContentLoaded', function () {
+    const forms = document.getElementById('form');
+    const listaTarefas = document.getElementById('tarefas');
+
+    function addTarefa(tituloTarefa, descTarefa) {
+        const tarefa = {
+            titulo: tituloTarefa,
+            desc: descTarefa
+        };
+
+        let tarefas = JSON.parse(localStorage.getItem('tarefas')) || [];
+        tarefas.push(tarefa);
+        localStorage.setItem('tarefas', JSON.stringify(tarefas));
+
+        displayTarefa();
+    }
+
+    function displayTarefa() {
+        listaTarefas.innerHTML = '';
+
+        const tarefas = JSON.parse(localStorage.getItem('tarefas')) || [];
+
+        tarefas.forEach(tarefa => {
+            const li = document.createElement('li');
+            li.textContent = '${tarefa.titulo}: {$tarefa.desc}';
+            listaTarefas.appendChild(li);
+        })
+    }
+
+    forms.addEventListener('submit', function (event) {
+        event.preventDefault();
+
+        const tituloTarefa = document.getElementById('titulo').value;
+        const descTarefa = document.getElementById('texto').value;
+
+        addTarefa(tituloTarefa, descTarefa);
+        forms.reset();
+
+    })
+//})
